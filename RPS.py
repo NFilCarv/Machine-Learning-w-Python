d2581eca2b53b19102b2a3a087884c50b14f3621
Nuno Carvalho <nuno8b20@gmail.com>
2024-11-17 23:38:32 +0000

    My solution for Rock-Paper-Scissors project

--- RPS.py
+++ RPS.py
@@ -1,10 +1,28 @@
-# The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
-
 def player(prev_play, opponent_history=[]):
+    
+    counter = {'P': 'S', 'R': 'P', 'S': 'R'}
+
+    if not prev_play:
+        prev_play = 'R'
+
+    if not hasattr(player, "steps"):
+        player.steps = {}
+
     opponent_history.append(prev_play)
+    prediction = 'P'
 
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+    
+    if len(opponent_history) > 4:
+        last_five = "".join(opponent_history[-5:])
+        player.steps[last_five] = player.steps.get(last_five, 0) + 1
+        
+        possible = ["".join([*opponent_history[-4:], v]) for v in ['R', 'P', 'S']]
 
-    return guess
+        under = {k: player.steps[k] for k in possible if k in player.steps}
+
+        if under:
+            prediction = max(under, key=under.get)[-1:]
+
+    
+
+    return counter[prediction]
